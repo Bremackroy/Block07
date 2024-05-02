a169ada7138ce3deddff51e75420d837cc36dbdb
Breanna Mackroy <breannamackroy@Breannas-MBP.home>
2024-05-01 20:14:28 -0400

    Updated Block07

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
-function displayAlert(strcomplete) {alert("strcomplete");   
-}
 const str1 = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
-let combination1 = 2 + 2 + 6;
-let combination2 = 80 / 2;
-let combination3 = 42 - 2 - 1;
+
+//This is the first arithmetic function/output for the combination. The result should be 10.
+var combination1 = 2 + 2 + 6;
+
+//This is the second arithmetic function/output for the combination. The result should be 40
+var combination2 = 80 / 2;
+
+//This is the first arithmetic function/output for the combination.The result should be 39.
+var combination3 = 42 - 2 - 1;
 const strcomplete = '${str1} ${combination1} ${combination2} ${combination3}!';
 
 console.log(strcomplete);
